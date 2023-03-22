4354b334d44b7ef7e24263945a23dc2f868f0f95
sushi-chaaaan <mail@sushichan.live>
2023-03-23 00:22:47 +0900

    progress. but this is meaningless. see #1

--- src/spotify.ts
+++ src/spotify.ts
@@ -9,10 +9,18 @@ spotifyRoute.get('/', (ctx) => {
   const client = new spotifyClient()
 
   // FIXME: ここをなんとか非同期実行にしないといけない
-  client.requestToken({
-    clientId: ctx.env.SPOTIFY_CLIENT_ID,
-    clientSecret: ctx.env.SPOTIFY_CLIENT_SECRET,
-  })
+  client
+    .requestToken({
+      clientId: ctx.env.SPOTIFY_CLIENT_ID,
+      clientSecret: ctx.env.SPOTIFY_CLIENT_SECRET,
+    })
+    .then((token) => {
+      console.log('token generated')
+      console.log(token)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 
   return ctx.text('Hello Hono!')
 })
